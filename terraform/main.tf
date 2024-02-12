000a7d0644026506ea0fe87d54de726d2e878cd4
Rui Mota <Rui.Filipe.Mota@criticaltechworks.com>
2024-02-12 15:11:24 +0000

    temp

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,86 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
 }
+
+resource "azurerm_public_ip" "myterraformpublicip" {
+  name                = "${var.prefix}-publicip"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+  domain_name_label = "vnet${random_string.number.result}"
+}
+
+
+
+variable "prefix" {
+  default = "tfvmex"
+}
+
+
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+  }
+}
+
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = var.resource_group_location
+  size                  = "Standard_DS1_v2"
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,5 @@
 output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.myterraformvm.public_ip_address
+  value = azurerm_virtual_machine.main.public_ip_address
 }
 
 output "account_id" {
