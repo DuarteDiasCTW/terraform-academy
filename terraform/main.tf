8405fe537d1e797c56141d8749899314d248ec29
Rui Mota <Rui.Filipe.Mota@criticaltechworks.com>
2024-02-14 15:02:12 +0000

    ansible config

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,6 +71,18 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "NodeExporter"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9100"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 #Create Network Interface
